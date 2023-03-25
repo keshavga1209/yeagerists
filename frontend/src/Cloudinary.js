4127a09d466129d4dcf25d46d4323f9415a8bdaa
neel-thakker <neelthakker2001@gmail.com>
2023-03-25 15:36:36 +0530

    indentation done to all files

--- frontend/src/Cloudinary.js
+++ frontend/src/Cloudinary.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 import { Cloudinary } from "cloudinary-core";
 
 export default function Cloudinary() {
